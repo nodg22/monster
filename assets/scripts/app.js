958ec920a6ba7bb12a094cde5eb338d6d4236ae6
Michał Doniec <doniecster@gmail.com>
2021-09-23 16:43:53 +0200

    win/lose conditionals

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,30 +1,31 @@
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VAL = 14;
-let chosenmaxLife, currentMonsterhealth, currentPlayerhealth; 
-
-
+let chosenmaxLife, currentMonsterhealth, currentPlayerhealth;
 
 chosenmaxLife = 100;
 currentMonsterhealth = chosenmaxLife;
 currentPlayerhealth = chosenmaxLife;
 
 adjustHealthBars(chosenmaxLife);
-const winCondition = ()=>{
-    if ( currentMonsterhealth <= 0){
-        alert('You Won!');
-    }
-    if(currentPlayerhealth <=0){
-        alert('You Lost!')
-    }
-
-}
+const winCondition = () => {
+  if (currentMonsterhealth <= 0 && currentPlayerhealth > 0) {
+    alert('You Won!');
+  }
+  if (currentPlayerhealth <= 0 && currentMonsterhealth > 0) {
+    alert('You Lost!');
+  }
+  if (currentMonsterhealth <= 0 && currentPlayerhealth <= 0) {
+    alert('Draw');
+  } else {
+  }
+};
 const attackhandler = () => {
-   const monsterDamage=  dealMonsterDamage(ATTACK_VALUE);
-   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VAL);
+  const monsterDamage = dealMonsterDamage(ATTACK_VALUE);
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VAL);
 
-   currentMonsterhealth -= monsterDamage;
-   currentPlayerhealth -= playerDamage;
-   winCondition();
+  currentMonsterhealth -= monsterDamage;
+  currentPlayerhealth -= playerDamage;
+  winCondition();
 };
 
 attackBtn.addEventListener('click', attackhandler);
