fbdda7324a0ce719f9778a3a4893da7ea4b3d560
Michał Doniec <doniecster@gmail.com>
2021-09-23 14:55:28 +0200

    added attack funcionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,18 @@
 const ATTACK_VALUE = 10;
-let  chosenmaxLife = 100;
+let chosenmaxLife, currentMonsterhealth, currentPlayerhealth; 
 
-adjuadjustHealthBars(chosenmaxLife);
 
-const attackhandler = ()=>{}
+
+chosenmaxLife = 100;
+currentMonsterhealth = chosenmaxLife;
+currentPlayerhealth = chosenmaxLife;
+
+adjustHealthBars(chosenmaxLife);
+
+const attackhandler = () => {
+   const damage=  dealMonsterDamage(ATTACK_VALUE);
+
+   currentMonsterhealth -= damage;
+};
 
 attackBtn.addEventListener('click', attackhandler);
